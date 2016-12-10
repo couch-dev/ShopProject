0214d2ed18468d03235c3f19f81f0f081884e658
couchdev <couch.develop@gmail.com>
2016-12-10 15:30:01 +0100

    updated profile fragment

--- app/src/main/java/net/couchdev/android/layoutsandbox/MainActivity.java
+++ app/src/main/java/net/couchdev/android/layoutsandbox/MainActivity.java
@@ -1,13 +1,15 @@
 package net.couchdev.android.layoutsandbox;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,7 +18,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import generated.ScrollingActivity;
@@ -24,21 +34,12 @@ import generated.ScrollingActivity;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    /**
-     * The number of pages (wizard steps) to show in this demo.
-     */
-    private static final int NUM_PAGES = 2;
+    private RelativeLayout contentMain;
 
-    /**
-     * The pager widget, which handles animation and allows swiping horizontally to access previous
-     * and next wizard steps.
-     */
-    private ViewPager mPager;
-
-    /**
-     * The pager adapter, which provides the pages to the view pager widget.
-     */
-    private PagerAdapter mPagerAdapter;
+    private enum Tab{
+        SHOP,
+        PROFILE
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +60,8 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        // navigation view
         final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         View headerView = navigationView.getHeaderView(0);
@@ -73,28 +76,145 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        // Instantiate a ViewPager and a PagerAdapter.
-        mPager = (ViewPager) findViewById(R.id.tabPager);
-        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
-        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-            @Override
-            public void onPageSelected(int position) {
-                if(position == 0){
-                    navigationView.setCheckedItem(R.id.nav_tab_shop);
-                } else {
-                    navigationView.setCheckedItem(R.id.nav_tab_profile);
-                }
-            }
-            @Override
-            public void onPageScrollStateChanged(int state) {
-            }
-        });
-        mPager.setAdapter(mPagerAdapter);
+        contentMain = (RelativeLayout) findViewById(R.id.content_main);
+        selectTab(Tab.SHOP);
 
         navigationView.setCheckedItem(R.id.nav_tab_shop);
+    }
+
+    private void selectTab(Tab tab){
+        switch (tab){
+            case SHOP: contentMain.removeAllViews();
+                contentMain.addView(LayoutInflater.from(this).inflate(R.layout.fragment_shop, null));
+                createShopFragment();
+                break;
+            case PROFILE: contentMain.removeAllViews();
+                contentMain.addView(LayoutInflater.from(this).inflate(R.layout.fragment_profile, null));
+                createProfileFragment();
+                break;
+        }
+    }
+
+    private void createShopFragment(){
+        ListView listView = (ListView) findViewById(R.id.salesList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.item_sale, R.id.itemTitle,
+                new String[]{"Shop Item 1", "Shop Item 2", "Shop Item 3", "Shop Item 4", "Shop Item 5"});
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, SaleItemActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final SearchView shopSearch = (SearchView) findViewById(R.id.shopSearch);
+        shopSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shopSearch.setIconified(false);
+            }
+        });
+    }
+
+    public void createProfileFragment() {
+        RecyclerView recycler = (RecyclerView) findViewById(R.id.myItemsRecycler);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recycler.setLayoutManager(layoutManager);
+        RecyclerView.Adapter adapter = new RecyclerView.Adapter<CustomViewHolder>(){
+            @Override
+            public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_profile, parent, false);
+                return new CustomViewHolder(view);
+            }
+            @Override
+            public void onBindViewHolder(CustomViewHolder holder, int position) {
+                // viewHolder.field.setText(mItems.get(i));
+            }
+            @Override
+            public int getItemCount() {
+                return 5;
+            }
+        };
+        recycler.setAdapter(adapter);
+
+        RecyclerView recycler2 = (RecyclerView) findViewById(R.id.favoRecycler);
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recycler2.setLayoutManager(layoutManager2);
+        RecyclerView.Adapter adapter2 = new RecyclerView.Adapter<CustomViewHolder>(){
+            @Override
+            public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_profile, parent, false);
+                TextView owner = (TextView) view.findViewById(R.id.itemOwner);
+                owner.setVisibility(View.VISIBLE);
+                return new CustomViewHolder(view);
+            }
+            @Override
+            public void onBindViewHolder(CustomViewHolder holder, int position) {
+            }
+            @Override
+            public int getItemCount() {
+                return 5;
+            }
+        };
+        recycler2.setAdapter(adapter2);
+
+        final RecyclerView recycler3 = (RecyclerView) findViewById(R.id.shopRecycler);
+        LinearLayoutManager layoutManager3 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recycler3.setLayoutManager(layoutManager3);
+        RecyclerView.Adapter adapter3 = new RecyclerView.Adapter<CustomViewHolder>(){
+            @Override
+            public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_profile, parent, false);
+                TextView owner = (TextView) view.findViewById(R.id.itemOwner);
+                owner.setVisibility(View.VISIBLE);
+                return new CustomViewHolder(view);
+            }
+            @Override
+            public void onBindViewHolder(CustomViewHolder holder, int position) {
+            }
+            @Override
+            public int getItemCount() {
+                return 5;
+            }
+        };
+        recycler3.setAdapter(adapter3);
+
+        final TextView shopHeader = (TextView) findViewById(R.id.shopHeader);
+        shopHeader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(recycler3.getVisibility() == View.INVISIBLE){
+                    recycler3.setVisibility(View.VISIBLE);
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) shopHeader.getLayoutParams();
+                    params.removeRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+                    shopHeader.setLayoutParams(params);
+                    ImageView arrow = (ImageView) findViewById(R.id.arrowImage);
+                    arrow.setImageDrawable(getResources().getDrawable(R.drawable.ic_arrow_down_small));
+                } else{
+                    recycler3.setVisibility(View.INVISIBLE);
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) shopHeader.getLayoutParams();
+                    params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+                    shopHeader.setLayoutParams(params);
+                    ImageView arrow = (ImageView) findViewById(R.id.arrowImage);
+                    arrow.setImageDrawable(getResources().getDrawable(R.drawable.ic_arrow_up_small));
+                }
+            }
+        });
+
+        ProgressBar buyerProgress = (ProgressBar) findViewById(R.id.buyerProgress);
+        buyerProgress.setMax(100);
+        buyerProgress.setProgress(60);
+        ProgressBar sellerProgress = (ProgressBar) findViewById(R.id.sellerProgress);
+        sellerProgress.setMax(100);
+        sellerProgress.setProgress(40);
+    }
+
+    private class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 
     @Override
@@ -112,10 +232,10 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.nav_tab_shop:
-                mPager.setCurrentItem(0, false);
+                selectTab(Tab.SHOP);
                 break;
             case R.id.nav_tab_profile:
-                mPager.setCurrentItem(1, false);
+                selectTab(Tab.PROFILE);
                 break;
             case R.id.nav_new_item:
                 Intent intent = new Intent(MainActivity.this, CreateItemActivity.class);
@@ -127,27 +247,4 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    /**
-     * A simple pager adapter that represents {@link #NUM_PAGES} ScreenSlidePageFragment objects, in
-     * sequence.
-     */
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-        public ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            if(position == 0){
-                return new ShopFragment();
-            } else {
-                return new ProfileFragment();
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return NUM_PAGES;
-        }
-    }
 }
